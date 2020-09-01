8fbe01b07a87bd21a166650b747ae6a9b0301e95
Ethan Malenchek <emalenchek@gmail.com>
2020-09-01 09:27:20 -0400

    Added Covid Tracker app to projects

--- testapp/src/components/AboutMe.js
+++ testapp/src/components/AboutMe.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card } from 'reactstrap';
 import grocery from '../grocery-project.JPG';
+import covid from '../covid-stat-tracker.JPG';
 
 class AboutMe extends Component {
     state = {
@@ -20,12 +21,12 @@ class AboutMe extends Component {
                                 
                             <div>
                                 <p className="text-center" style={{fontSize: 1.25 + 'em'}}>
-                                    Hi! My name is Ethan Malenchek, and I am a senior
-                                    studying Computer Science at Ohio University. I 
+                                    Hi! My name is Ethan Malenchek, and I am a senior at Ohio University
+                                    studying Computer Science. I 
                                     plan to graduate in May 2021.
                                 </p>
                                 <p className="text-center" style={{fontSize: 1.25 + 'em'}}>
-                                    I have 2 years of 
+                                    I have 3 years of 
                                     HTML/CSS experience and solid foundations in 
                                     JavaScript. These foundations include Vanilla JS 
                                     and ES6, as well as libraries and frameworks including: 
@@ -45,10 +46,19 @@ class AboutMe extends Component {
                             </div>
                                 
                             <div>
-                                <Card>
+                                <Card className="mb-5">
                                     <div>                                       
-                                        <img src={grocery} alt="Grocery Project" class="img-fluid" />
+                                        <img src={grocery} alt="Grocery Project" className="img-fluid" />
                                         <h3 className="text-center"><a href="https://salty-tor-54455.herokuapp.com/">Grocery List Application</a></h3>
+                                        <p className="text-center"></p>
+                                    </div>
+                                </Card>
+                            </div>
+                            <div>
+                                <Card className="mb-5">
+                                    <div>                                       
+                                        <img src={covid} alt="Covid Stats Tracker" className="img-fluid" />
+                                        <h3 className="mt-5 text-center"><a href="https://covid-stats-tracker.herokuapp.com/">COVID Stats Tracker</a></h3>
                                         <p className="text-center"></p>
                                     </div>
                                 </Card>
